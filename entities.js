84233a3b42d27dcdb62d6956449c1d257bf0ffcf
arielcolab <133857600+arielcolab@users.noreply.github.com>
2025-07-26 00:29:21 +0300

    Add files via upload

---
+++ entities.js
@@ -0,0 +1,121 @@
+import { base44 } from './base44Client';
+
+
+export const Dish = base44.entities.Dish;
+
+export const Order = base44.entities.Order;
+
+export const Address = base44.entities.Address;
+
+export const Review = base44.entities.Review;
+
+export const Favorite = base44.entities.Favorite;
+
+export const Transaction = base44.entities.Transaction;
+
+export const Report = base44.entities.Report;
+
+export const SavedDish = base44.entities.SavedDish;
+
+export const Activity = base44.entities.Activity;
+
+export const Follow = base44.entities.Follow;
+
+export const CookingClass = base44.entities.CookingClass;
+
+export const MealPlan = base44.entities.MealPlan;
+
+export const ShoppingListItem = base44.entities.ShoppingListItem;
+
+export const CommunityGroup = base44.entities.CommunityGroup;
+
+export const GroupMembership = base44.entities.GroupMembership;
+
+export const Gift = base44.entities.Gift;
+
+export const Recipe = base44.entities.Recipe;
+
+export const FreeMeal = base44.entities.FreeMeal;
+
+export const Message = base44.entities.Message;
+
+export const SupportRequest = base44.entities.SupportRequest;
+
+export const Membership = base44.entities.Membership;
+
+export const StripeAccount = base44.entities.StripeAccount;
+
+export const Payment = base44.entities.Payment;
+
+export const Payout = base44.entities.Payout;
+
+export const dd_Cook = base44.entities.dd_Cook;
+
+export const dd_Order = base44.entities.dd_Order;
+
+export const dd_Payout = base44.entities.dd_Payout;
+
+export const dd_SavedSearch = base44.entities.dd_SavedSearch;
+
+export const dd_LoyaltyPoints = base44.entities.dd_LoyaltyPoints;
+
+export const dd_CommunityPost = base44.entities.dd_CommunityPost;
+
+export const dd_BulkUploadJob = base44.entities.dd_BulkUploadJob;
+
+export const dd_DishAllergen = base44.entities.dd_DishAllergen;
+
+export const dd_DeliveryQuote = base44.entities.dd_DeliveryQuote;
+
+export const dd_Credits = base44.entities.dd_Credits;
+
+export const dd_LeaderboardWeekly = base44.entities.dd_LeaderboardWeekly;
+
+export const dd_MediaCache = base44.entities.dd_MediaCache;
+
+export const MealPrepService = base44.entities.MealPrepService;
+
+export const DeliveryService = base44.entities.DeliveryService;
+
+export const FreeFoodGiveaway = base44.entities.FreeFoodGiveaway;
+
+export const Giveaway = base44.entities.Giveaway;
+
+export const LastCallEat = base44.entities.LastCallEat;
+
+export const SurplusGrocery = base44.entities.SurplusGrocery;
+
+export const DD2_User = base44.entities.DD2_User;
+
+export const DD2_Recipe = base44.entities.DD2_Recipe;
+
+export const DD2_Restaurant = base44.entities.DD2_Restaurant;
+
+export const Profile = base44.entities.Profile;
+
+export const Restaurant = base44.entities.Restaurant;
+
+export const UserPoints = base44.entities.UserPoints;
+
+export const Badge = base44.entities.Badge;
+
+export const UserBadge = base44.entities.UserBadge;
+
+export const Challenge = base44.entities.Challenge;
+
+export const UserChallenge = base44.entities.UserChallenge;
+
+export const SellerProfile = base44.entities.SellerProfile;
+
+export const Listing = base44.entities.Listing;
+
+export const OrderItem = base44.entities.OrderItem;
+
+export const StatusEvent = base44.entities.StatusEvent;
+
+export const AnalyticsEvent = base44.entities.AnalyticsEvent;
+
+
+
+// auth sdk:
+export const User = base44.auth;
